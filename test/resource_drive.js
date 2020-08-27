0b64c81b6964991d8b980cf07e4f7f22559a5b53
HorakR <37213063+HorakR@users.noreply.github.com>
2020-08-27 16:03:01 +0200

    resource sql

--- test/resource_drive.js
+++ test/resource_drive.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Cloud = require('../lib/cloud.js');
+const SQL = require('liqd-sql');
 
 async function test_drive( uid )
 {
@@ -9,4 +10,31 @@ async function test_drive( uid )
     console.log( data.root );
 }
 
+async function test_sql( uid )
+{
+    let data = await Cloud.Resource.SQL( uid, { type: 'mysql' } );
+
+    if( data )
+    {
+        let DB = new SQL({
+            mysql: {
+                host     : 'localhost',
+                user     : uid + '_admin',
+                password : 'hrasko1',
+                database : uid
+            }
+        });
+
+        let query = await DB.query( 'SELECT 1 FROM DUAL' ).execute();
+
+        if( query.ok )
+        {
+            console.log( 'SQL test passed', query );
+        }
+        else { console.log( 'SQL test failed' ); }
+    }
+    else { console.log( 'DB creating failed' ); }
+}
+
 test_drive( 'test_2' );
+test_sql( 'janko1' );
